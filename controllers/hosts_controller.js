158f57f90e49018d55b76be14bb67cbb608824a1
SamarSayed <samarsayed998@gmail.com>
2021-02-22 02:58:23 +0200

    hosts second commit

--- controllers/hosts_controller.js
+++ controllers/hosts_controller.js
@@ -1,36 +1,87 @@
 const Hosts = require("../models/host");
-module.exports={
+const {
+    add: addPlace
+} = require("../controllers/places_controllers");
+module.exports = {
 
-all(req,res,next){
-    const userId = req.user.id;
-    Hosts.findOne({user_id:userId})
-    .then((hosts) =>{
-        let hostingIds = hosts["listing"]
-        res.status(200).send(hostingIds)
-    })
-    .catch(next);
-},
-add(req,res,next){
+    all(req, res, next) {
+        const userId = req.user.id;
+        Hosts.findOne({
+                user_id: userId
+            })
+            .then((hosts) => {
+                let hostingIds = hosts["listings"]
+                res.status(200).send(hostingIds)
+            })
+            .catch(next);
+    },
 
-},
-delete(req,res,next){
-    const userId = req.user.id;
-    const placeId = req.params.id;
-    Hosts.findOne({user_id:userId})
-    .then((hosts) =>{
-        for (let index = 0; index < hosts["listing"]["place_ids"].length; index++) {
-            let palce = hosts["listing"]["place_ids"][index]["place_id"]
-            if (palce==placeId) {
-                hosts["listing"]["place_ids"].splice(index,1)
-                console.log("deleted")
-                break;
+
+    async add(req, res, next) {
+        try {            
+            placeId=req.placeId
+            const userId = req.user.id;
+            hosts = await Hosts.findOne({
+                user_id: userId
+            })
+
+            if (hosts) {
+                hosts["listings"]["place_ids"].push({
+                    place_id: placeId,
+                    start_date: new Date(),
+                    end_date: new Date()
+                })
+            
+                await Hosts.findByIdAndUpdate({
+                    _id: hosts["_id"]
+                }, hosts)
+                res.status(200).send(hosts);
+            } 
+            else {
+                console.log("hosts not exist")
+                host = new Hosts({
+                    user_id: userId,
+                    listings: {
+                        place_ids: [{
+                            place_id: placeId,
+                            start_date: new Date(),
+                            end_date: new Date()
+                        }],
+                        experience_ids: []
+                    }
+                })
+                await host.save();
+                res.status(200).send(host);
             }
+        } catch (err) {
+            console.log(err)
         }
-    Hosts.findByIdAndUpdate({_id:hosts["_id"]},hosts).then(()=>{
-        res.status(204).json({ message: "Deleted Successfully" })
-    })
-    })
-    .catch(next);
-}
+    },
+
+
+
+    delete(req, res, next) {
+        const userId = req.user.id;
+        const placeId = req.params.id;
+        Hosts.findOne({
+                user_id: userId
+            })
+            .then((hosts) => {
+                for (let index = 0; index < hosts["listings"]["place_ids"].length; index++) {
+                    let palce = hosts["listings"]["place_ids"][index]["place_id"]
+                    if (palce == placeId) {
+                        hosts["listings"]["place_ids"].splice(index, 1)
+                        console.log("deleted")
+                        break;
+                    }
+                }
+                Hosts.findByIdAndUpdate({
+                    _id: hosts["_id"]
+                }, hosts).then(() => {
+                next();
+                })
+            })
+            .catch(next);
+    }
 
 };
--- routes/hosts_routes.js
+++ routes/hosts_routes.js
@@ -3,13 +3,14 @@ const hostsController = require('../controllers/hosts_controller');
 const express = require('express');
 const router = express.Router();
 const {users_auth} = require('../controllers/users_controller');
+const {add:addPlace , delete:deletePlace} = require("../controllers/places_controllers")
 
 
 // hosting routes
 
 router.get('/',users_auth,hostsController.all);
-router.post('/hostPlace',users_auth,hostsController.add);
-router.delete('/deletePlace/:id',users_auth,hostsController.delete);
+router.post('/hostPlace',[users_auth,addPlace],hostsController.add);
+router.delete('/deletePlace/:id',[users_auth,hostsController.delete],deletePlace);
 
 
 module.exports = router;
