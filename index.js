64d1e59027ddc04cb503a2410fa20d18a79ef6e1
omar <omar96_96@hotmail.com>
2021-02-24 23:53:23 +0200

    Solving CORS Policy problem

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require('./src/app.js');
 
 const PORT =  process.env.PORT || 4000;
 app.listen(PORT, ()=>{
-  console.log('Server is currently running on port 4000');
+  console.log(`Server is currently running on port ${PORT}`);
 })
