cafb7030d2aae2507340a280bd3547311bad23d7
Ahmed <elmoazen13@gmail.com>
2021-03-06 16:10:06 +0200

    backup branch

--- routes/reservations_routes.js
+++ routes/reservations_routes.js
@@ -6,6 +6,6 @@ router.post("/add/new", reservationsController.add);
 router.get("/res/:id", reservationsController.fetchRes);
 router.get("/forplace/:id", reservationsController.fetchAllForPlace);
 router.get("/foruser/:id", reservationsController.fetchAllForUser);
-router.delete("/res/:id", reservationsController.deleteRes);
+router.delete("/deleteres/:id", reservationsController.deleteRes);
 
 module.exports = router; 
